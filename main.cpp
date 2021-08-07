ac2a601c0abcd5272b64b8c3f3b7831376a6c111
Ricardo Asin <ricardoasinr@gmail.com>
2021-08-06 23:32:05 -0400

    Solo hay un hola mundo

---
+++ main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  Basic C++
+//
+//  Created by Ricardo Asin on 6/8/21.
+//
+
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    
+    
+    return 0;
+}
