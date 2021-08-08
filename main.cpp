217a861fac4b9517b1543a4e3fa7ded67085e46a
Ricardo Asin <ricardoasinr@gmail.com>
2021-08-07 23:13:04 -0400

    Anadiendo funcion mayor

--- main.cpp
+++ main.cpp
@@ -4,6 +4,15 @@
 //
 //  Created by Ricardo Asin on 6/8/21.
 //
+int mayor (int var1, int var2)
+{
+    int mayy =0;
+    if(var1>var2)
+    {
+         mayy = var1;
+    }
+    return  mayy;
+}
 
 #include <iostream>
 using namespace std;
@@ -28,6 +37,7 @@ int main(int argc, const char * argv[]) {
     //Añadiendo comentarios
     //f8ßß∂
     //Y mi anterior codigo?
-    
+    int m = mayor(a, b);
+    cout<<m;
     return 0;
 }
